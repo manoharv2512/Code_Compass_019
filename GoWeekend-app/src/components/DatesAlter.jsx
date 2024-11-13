3949cd648645f2590e7f5138154224db193a7057
i-am-shank <shashanksinha4444@gmail.com>
2024-11-13 21:31:55 +0530

    Initial setup of Main Trip-page & it's initial 2-3 components, added.

---
+++ GoWeekend-app/src/components/DatesAlter.jsx
@@ -0,0 +1,9 @@
+// initial setup of DatesAlter-component (main trip-page)
+
+export default function DatesAlter() {
+    return (
+        <>
+            <h1>This is Dates-Alter component (main trip-page).</h1>
+        </>
+    );
+}
---
+++ GoWeekend-app/src/components/Overview.jsx
@@ -0,0 +1,9 @@
+// initial setup of Overview-component (trip-page)
+
+export default function Overview() {
+    return (
+        <>
+            <h1>This is Overview-component (of main trip-page).</h1>
+        </>
+    );
+}
