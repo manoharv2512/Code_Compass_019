0893a5d247506909945fe21ed51eca439671b742
Manohar Varma <varmamanohar1998@gmail.com>
2024-11-16 08:55:17 +0530

    landing page work in progress

--- GoWeekend-app/src/main.jsx
+++ GoWeekend-app/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LandingPage from './pages/LandingPage.jsx'
+import Article1 from './components/Article1.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <LandingPage/>
     <App />
+    <Article1 />
   </StrictMode>,
 )
